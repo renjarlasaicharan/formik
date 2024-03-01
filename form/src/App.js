f04d240d09ac09710aebec1bd6bb73b044a89475
sai charan <saicharanrenjarla2000@gmail.com>
2024-03-01 16:12:44 +0530

    first commit

---
+++ form/src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import './App.css';
+import Formfield from "./Formfield"
+function App() {
+  return (
+    <div className="App">
+     <Formfield/>
+    </div>
+  );
+}
+
+export default App;
---
+++ form/src/Formfield.js
@@ -0,0 +1,61 @@
+import React from "react"
+import ReactDOM from "react-dom";
+import { useFormik, Field, Form,Formik } from "formik";
+import * as Yup from 'yup';
+
+const initialValues={
+  name:'',
+  email:'',
+  password:'',
+  cpassword:''
+}
+
+const  Formfield=()=> {
+
+ const validationSchema=Yup.object({
+    name: Yup.string().min(3).required('Please Enter name'),
+    email: Yup.string().email( 'Please Enter Valid email').required('Please Enter Email'),
+    password: Yup.string().min(5).required('Plese Enter Password'),
+    cpassword: Yup.string().oneOf([Yup.ref('password')],'Password not matched').required('Please Enter c Password')
+  })
+  
+
+  const {values,handleBlur,handleChange,handleSubmit,errors,touched} = useFormik({
+    initialValues:initialValues,
+    validationSchema:validationSchema,
+    onSubmit:(values)=>{
+      console.log(values)
+    }
+  })
+
+  return (
+    <div className="App">
+        <form onSubmit={handleSubmit}>
+          <label>Name:</label><br/>
+          <input name="name" type="text" onBlur={handleBlur} onChange={handleChange} value={values.name} />
+          <br/>
+          {touched.name && <small>{errors.name}</small>}
+<br/>
+          <label>email:</label><br/>
+          <input name="email" type="email" onBlur={handleBlur} onChange={handleChange} value={values.email} />
+          <br/>
+          {touched.email && <small>{errors.email}</small>}
+<br/>
+          <label>PassWord:</label><br/>
+          <input name="password" type="password" onBlur={handleBlur} onChange={handleChange} value={values.password} />
+          <br/>
+          {touched.password && <small>{errors.password}</small>}
+<br/>
+          <label>Confirm PassWord:</label><br/>
+          <input name="cpassword" type="password" onBlur={handleBlur} onChange={handleChange} value={values.cpassword} />
+          <br/>
+          {touched.cpassword && <small>{errors.cpassword}</small>}
+          <br/>
+          <button type="submit">Submit</button>
+          <br/>
+        </form>
+
+    </div>
+  );
+}
+export default Formfield
